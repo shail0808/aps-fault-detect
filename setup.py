7afcb41d93f61d8ca62c15b4919e246fef8679c0
shail0808 <shail080881@gmail.com>
2022-12-03 12:48:48 +0530

    update

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+
+
+
+setup(
+    name='sensor',
+    version = '0.0.1',
+    author = 'shailesh',
+    author_email = 'shail080881@gmail.com',
+    packages= find_packages(),
+    install_requires = get_requirements()
+
+)
+
+
+
+
+
